bc38536c31215d2f0df672f43a071a526a722df9
Sauli Nevalainen <nevalainen.sauli@gmail.com>
2021-11-17 02:05:22 +0200

    Ex 1.05

--- part1/project/src/index.js
+++ part1/project/src/index.js
@@ -1,7 +1,14 @@
 const Koa = require('koa')
+const Router = require('@koa/router')
 const app = new Koa()
+const router = new Router()
 const port = process.env.HTTP_PORT || 3000
 
+router.get('/', (ctx, next) => {
+    ctx.body = 'Hello World!'
+})
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(port)
 console.log(`Server started in port ${port}`)
