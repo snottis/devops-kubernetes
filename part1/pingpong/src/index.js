757c2d4323961597e163886ea7015af2fea607a5
Sauli Nevalainen <nevalainen.sauli@gmail.com>
2021-11-17 20:43:20 +0200

    1.09

---
+++ part1/pingpong/src/index.js
@@ -0,0 +1,17 @@
+const Koa = require('koa')
+const Router = require('@koa/router')
+const app = new Koa()
+const router = new Router()
+const port = process.env.HTTP_PORT || 3000
+
+let pings = 0
+
+router.get('/', (ctx, next) => {
+    ctx.body = `pong ${pings}`
+    ++pings
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(port)
+console.log(`Server started in port ${port}`)
